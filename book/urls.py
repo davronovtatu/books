d39aa800a043b5ef700ea5548db9e250a8103ef8
davronovtatu <davronovtatu@gmail.com>
2024-01-16 16:45:59 +0500

    detail sahifa qo'shildi

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView,ContactView,BookListView
+from .views import HomeView,ContactView,BookListView,BookDetailView
 
 urlpatterns=[
     path('',HomeView,name='home'),
     path('aloqa/',ContactView,name='acloqa'),
     path('booklist/',BookListView,name='listb'),
+    path('detail/<int:id>/',BookDetailView,name='det'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 def HomeView(request):
@@ -17,4 +17,14 @@ def BookListView(request):
         "books":books
     }
     return render(request,"books-media-gird-view-v2.html",context)
+
+
+
+def BookDetailView(request,id):
+    book=get_object_or_404(Book,id=id)
+    context={
+        "book":book
+    }
+    # return render(request,"detail.html",context)
+    return render(request,"books-media-detail-v2.html",context)
 
